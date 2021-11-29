491b18c26992d6b08b4ced6dfa52524ec5465b32
Hichami Houssam <hichami.houssam@gmail.com>
2021-11-29 16:58:38 +0100

    creating animals table

--- schema.sql
+++ schema.sql
@@ -2,6 +2,13 @@
 
 CREATE DATABASE vet_clinic;
 
+\c vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+    id int NOT NULL,
+    name varchar(20) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL DEFAULT false,
+    weight_kg decimal(5,2) NOT NULL
 );
