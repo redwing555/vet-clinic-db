f91aa3012646a8dacc9cb4e8a63ec423150f2119
Hichami Houssam <hichami.houssam@gmail.com>
2021-11-29 17:57:04 +0100

    setting id as primary key

--- schema.sql
+++ schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE vet_clinic;
 \c vet_clinic;
 
 CREATE TABLE animals (
-    id int NOT NULL,
-    name varchar(20) NOT NULL,
+    id int NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name varchar(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL DEFAULT false,
