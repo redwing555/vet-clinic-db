af7d67298f11eeee4c12bd70d32faa2dcad6c388
Hichami Houssam <hichami.houssam@gmail.com>
2021-11-30 10:53:51 +0100

    populating animals table with new data

--- data.sql
+++ data.sql
@@ -12,4 +12,22 @@ VALUES ('Pikachu','07/01/2021',1,false,15.04);
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES ('Devimon','12/05/2017', 5,true,11);
 
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Charmander','08/02/2020', 0,false,-11);
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Plantmon','15/11/2022', 2,true,-5.7);
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Squirtle','02/04/1993', 3,false,-12.13);
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Angemon','12/06/2005', 1,true,-45);
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Boarmon','07/06/2005', 7,true,20.4);
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES ('Blossom','13/10/1998', 3,true,17);
+
 
